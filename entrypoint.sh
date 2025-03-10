fcd70ddc4af6d3cd524e20f2ac3ea54bc0d48120
Datadot <56523663+Jacobsjohagra@users.noreply.github.com>
2025-03-10 22:43:56 +0100

    Add support for github-enterprise

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,19 @@
 #!/usr/bin/env bash
 
-# Expand the arguments into an array of strings. This is required because the GitHub action
-# provides all arguments concatenated as a single string.
+# Expand the arguments into an array of strings
 ARGS=("$@")
 
+GITHUB_DOMAIN="${GITHUB_CUSTOM_DOMAIN:-github.com}"
+
 if [[ ! -z "${GITHUB_AUTHENTICATION_TOKEN}" ]]; then
-  git config --global --add url."https://x-access-token:${GITHUB_AUTHENTICATION_TOKEN}@github.com/".insteadOf "https://github.com/"
+  git config --global --add url."https://x-access-token:${GITHUB_AUTHENTICATION_TOKEN}@${GITHUB_DOMAIN}/".insteadOf "https://${GITHUB_DOMAIN}/"
 fi
 
-/bin/gosec ${ARGS[*]}
+FILTERED_ARGS=()
+for arg in "${ARGS[@]}"; do
+  if [[ "$arg" != "GITHUB_CUSTOM_DOMAIN=${GITHUB_CUSTOM_DOMAIN}" ]]; then
+    FILTERED_ARGS+=("$arg")
+  fi
+done
+
+/bin/gosec "${FILTERED_ARGS[*]}"
